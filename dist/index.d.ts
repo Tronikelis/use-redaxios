47715a84b125c64964a6af0bf28908afbb8bf2f0
Tronikel <56039679+Trunkelis@users.noreply.github.com>
2021-09-26 20:52:57 +0300

    export Response type from redaxios

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,45 +1,47 @@
 import { Options, Response } from 'redaxios';
+export { Response } from 'redaxios';
 import { ReactNode } from 'react';
 
 interface useRedaxiosOptions<Body> {
-  interceptors?: {
-    request?: (request: Options) => Promise<Options>
-  }
-  onSuccess?: (res: Body) => void
-  onError?: (error: Response<any>) => void
-  axios?: Options
+    interceptors?: {
+        request?: (request: Options) => Promise<Options>;
+    };
+    onSuccess?: (res: Body) => void;
+    onError?: (error: Response<any>) => void;
+    axios?: Options;
 }
 
-type BodyMethod = <Data>(url: string, data: any) => Promise<Data>
-type BodylessMethod<T> = (url?: string) => Promise<T>
+type BodyMethod = <Data>(url: string, data: any) => Promise<Data>;
+type BodylessMethod<T> = (url?: string) => Promise<T>;
 
 interface useRedaxiosFnReturns<T> {
-  get: BodylessMethod<T>
-  del: BodylessMethod<T>
+    get: BodylessMethod<T>;
+    del: BodylessMethod<T>;
 
-  post: BodyMethod
-  put: BodyMethod
-  patch: BodyMethod
+    post: BodyMethod;
+    put: BodyMethod;
+    patch: BodyMethod;
 
-  loading: boolean
-  data: T | null
-  error: Response<any>
+    loading: boolean;
+    data: T | null;
+    error: Response<any>;
 }
 
-type RequestTypes = 'post' | 'get' | 'delete' | 'put' | 'patch'
+type RequestTypes = "post" | "get" | "delete" | "put" | "patch";
+
 
 declare function useRedaxios<T>(
-  url: string,
-  options: useRedaxiosOptions<T> = {},
-  deps?: any[]
-): useRedaxiosFnReturns<T>
+    url: string,
+    options?: useRedaxiosOptions<T>,
+    deps?: any[]
+): useRedaxiosFnReturns<T>;
 
 declare function FetchProvider({
-  options,
-  children
+    options,
+    children,
 }: {
-  options: useRedaxiosOptions<unknown>
-  children: ReactNode
-}): JSX.Element
+    options: useRedaxiosOptions<unknown>;
+    children: ReactNode;
+}): JSX.Element;
 
 export { FetchProvider, RequestTypes, useRedaxios, useRedaxiosFnReturns, useRedaxiosOptions };
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,43 +1,46 @@
-import { Options, Response } from 'redaxios'
-import { ReactNode } from 'react'
+import { Options, Response } from "redaxios";
+import { ReactNode } from "react";
+
+export { Response };
 
 export interface useRedaxiosOptions<Body> {
-  interceptors?: {
-    request?: (request: Options) => Promise<Options>
-  }
-  onSuccess?: (res: Body) => void
-  onError?: (error: Response<any>) => void
-  axios?: Options
+    interceptors?: {
+        request?: (request: Options) => Promise<Options>;
+    };
+    onSuccess?: (res: Body) => void;
+    onError?: (error: Response<any>) => void;
+    axios?: Options;
 }
 
-type BodyMethod = <Data>(url: string, data: any) => Promise<Data>
-type BodylessMethod<T> = (url?: string) => Promise<T>
+type BodyMethod = <Data>(url: string, data: any) => Promise<Data>;
+type BodylessMethod<T> = (url?: string) => Promise<T>;
 
 export interface useRedaxiosFnReturns<T> {
-  get: BodylessMethod<T>
-  del: BodylessMethod<T>
+    get: BodylessMethod<T>;
+    del: BodylessMethod<T>;
 
-  post: BodyMethod
-  put: BodyMethod
-  patch: BodyMethod
+    post: BodyMethod;
+    put: BodyMethod;
+    patch: BodyMethod;
 
-  loading: boolean
-  data: T | null
-  error: Response<any>
+    loading: boolean;
+    data: T | null;
+    error: Response<any>;
 }
 
-export type RequestTypes = 'post' | 'get' | 'delete' | 'put' | 'patch'
+export type RequestTypes = "post" | "get" | "delete" | "put" | "patch";
+
 
 export function useRedaxios<T>(
-  url: string,
-  options: useRedaxiosOptions<T> = {},
-  deps?: any[]
-): useRedaxiosFnReturns<T>
+    url: string,
+    options?: useRedaxiosOptions<T>,
+    deps?: any[]
+): useRedaxiosFnReturns<T>;
 
 export function FetchProvider({
-  options,
-  children
+    options,
+    children,
 }: {
-  options: useRedaxiosOptions<unknown>
-  children: ReactNode
-}): JSX.Element
+    options: useRedaxiosOptions<unknown>;
+    children: ReactNode;
+}): JSX.Element;
