57e50caab513931cc420b080649e619fc251f312
Donatas Bucinskas <donatas@tronikel.com>
2021-10-28 15:53:21 +0300

    add cjs module

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,17 +2,25 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import externals from "rollup-plugin-node-externals";
 import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
 
 import pkg from "./package.json";
 
 export default [
     {
         input: "./src/index.ts",
-        output: {
-            file: pkg.main,
-            format: "esm",
-        },
+        output: [
+            {
+                file: pkg.main,
+                format: "cjs",
+            },
+            {
+                file: pkg.module,
+                format: "esm",
+            },
+        ],
         plugins: [
+            commonjs(),
             typescript({
                 target: "ES2019",
                 jsx: "react-jsx",
